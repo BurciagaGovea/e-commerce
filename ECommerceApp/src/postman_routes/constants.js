b926013bb7867a946c0681060e1e47ce27913728
Alejandro Puentes de busk <alejandro@192.168.1.16>
2025-04-09 16:04:06 -0600

    Admin_profile_functional

--- ECommerceApp/src/postman_routes/constants.js
+++ ECommerceApp/src/postman_routes/constants.js
@@ -10,6 +10,9 @@ export const get_categories="http://192.168.1.16:8081/esb/categories"
 export const post_category ="http://192.168.1.16:8081/esb/categories"
 
 
+//
+export const get_orders="http://192.168.1.16:8081/esb/orders"
+
 
 //la estoy poniendo manual en lo que jala el login
-export const TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MywiZW1haWwiOiJudWV2b0BtYWlsLmNvbSIsInJvbGUiOm51bGwsImlhdCI6MTc0NDIyNTI3OSwiZXhwIjoxNzQ0MjI4ODc5fQ.6iLBdHYfXxn_FC1SdUtjN-7VehwHf3kEy4UZ1CYRCYo";
+export const TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MywiZW1haWwiOiJudWV2b0BtYWlsLmNvbSIsInJvbGUiOm51bGwsImlhdCI6MTc0NDIzNDM0NywiZXhwIjoxNzQ0MjM3OTQ3fQ.kCUoxLBBMwKWinSeK780lPKrN7YRzjdjrPjw0IEPmY4";
