bda2012ff5a60b72175b9383811cb3cda6462e7f
Alejandro Puentes de busk <alejandro@192.168.1.16>
2025-04-09 14:14:07 -0600

    functional_create_category

--- ECommerceApp/src/postman_routes/constants.js
+++ ECommerceApp/src/postman_routes/constants.js
@@ -7,6 +7,7 @@ export const get_products = "http://192.168.1.16:8081/esb/products";
 export const create_product = "http://192.168.1.16:8081/esb/products/upload"; 
 //get_categories
 export const get_categories="http://192.168.1.16:8081/esb/categories"
+export const post_category ="http://192.168.1.16:8081/esb/categories"
 
 
 
