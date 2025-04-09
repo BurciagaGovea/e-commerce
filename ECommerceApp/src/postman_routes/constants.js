aef5170577a9de08bf8f7ba94587a9b0967ae1cf
Alejandro Puentes de busk <alejandro@192.168.1.16>
2025-04-09 12:18:05 -0600

    edit_products_funcional

---
+++ ECommerceApp/src/postman_routes/constants.js
@@ -0,0 +1,10 @@
+
+//Nota solo va a jalar en i pc hasta que este la ruta de railway
+
+//Contante de get_products
+export const get_products = "http://192.168.1.16:8081/esb/products";
+
+
+
+//la estoy poniendo manual en lo que jala el login
+export const TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MywiZW1haWwiOiJudWV2b0BtYWlsLmNvbSIsInJvbGUiOm51bGwsImlhdCI6MTc0NDIyMTYwNywiZXhwIjoxNzQ0MjI1MjA3fQ.RHmG5kiUr2HCCaqjL3RIIYSkbTkWWDLxDIRa-a4JWAg";
