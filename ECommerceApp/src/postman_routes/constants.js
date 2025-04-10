04c6523f8a7d962783b34b58cb25a680952b750c
Alejandro Puentes de busk <alejandro@192.168.1.16>
2025-04-09 23:54:20 -0600

    rutas de railway

--- ECommerceApp/src/postman_routes/constants.js
+++ ECommerceApp/src/postman_routes/constants.js
@@ -1,26 +1,26 @@
 
 //Nota solo va a jalar en i pc hasta que este la ruta de railway
 
-export const login= "http://192.168.1.16:8081/esb/users/login";
+export const login= "https://eesb-production.up.railway.app/esb/users/login";
 
 export const register= "https://eesb-production.up.railway.app/esb/users/create"
 
 //Contante de get_products
-export const get_products = "http://192.168.1.16:8081/esb/products";
-export const create_product = "http://192.168.1.16:8081/esb/products/upload"; 
+export const get_products = "https://eesb-production.up.railway.app/esb/products";
+export const create_product = "https://eesb-production.up.railway.app/esb/products/upload"; 
 //get_categories
 
-export const get_categories="http://192.168.1.16:8081/esb/categories"
-export const post_category ="http://192.168.1.16:8081/esb/categories"
+export const get_categories="https://eesb-production.up.railway.app/esb/categories"
+export const post_category ="https://eesb-production.up.railway.app/esb/categories"
 
 
 //
-export const get_orders="http://192.168.1.16:8081/esb/orders"
-export const order_details="http://192.168.1.16:8081/esb/orders/details/"
+export const get_orders="https://eesb-production.up.railway.app/esb/orders"
+export const order_details="https://eesb-production.up.railway.app/esb/orders/details/"
 
 
 //
-export const get_clients='http://192.168.1.16:8081/esb/clients'
+export const get_clients='https://eesb-production.up.railway.app/esb/clients'
 
 
 
