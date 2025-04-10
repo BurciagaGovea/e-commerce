9e0b13b2c78eb0106c666004914e23c745f48e0f
Alejandro Puentes de busk <alejandro@192.168.1.16>
2025-04-09 19:45:00 -0600

    navegacion de login a home

--- ECommerceApp/App.js
+++ ECommerceApp/App.js
@@ -1,37 +1,12 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
-import TabNavigator from "./src/navigation/TabNavigator";
-import HomeScreen from "./src/screens/Home";
-import Charge from "./src/screens/Charge";
-import LoginScreen from "./src/screens/Login";
-import Wishlist from "./src/screens/WishList";
-import CartScreen from "./src/screens/Cart";
-import Search from "./src/screens/SearchPage";
-import ProductDetail from "./src/screens/DetailsProduct";
-import OrdersScreen from "./src/screens/OrdersScreen";
-import Profile from "./src/screens/Profile";
-import ProfileScreen from "./src/screens/Profile";
-import WelcomeScreen from "./src/screens/WelcomeScreen";
-import StackNavigator from "./src/navigation/StackNavigator";
-
+import AuthStack from "./src/navigation/AuthStack";
 
 export default function App() {
   return (
     <NavigationContainer>
-      {/* <StackNavigator/> */}
-      <TabNavigator/>
-      {/* <TabNavigator/> */}
-      {/* <Charge /> */}
-      {/* <LoginScreen/> */}
-      {/* <Wishlist/> */}
-      {/* <SignUpScreen/> */}
-      {/* <CartScreen/> */}
-      {/* <Search/> */}
-      {/* <ProductDetail/> */}
-      {/* <OrdersScreen/> */}
-      {/* <ProfileScreen/> */}
-      {/* <WelcomeScreen/> */}
+      <AuthStack />
     </NavigationContainer>
   );
-};
+}
 
