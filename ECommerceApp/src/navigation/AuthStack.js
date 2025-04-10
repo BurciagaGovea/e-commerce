7c254eb591b8c59dbe9e4cd3ad7c62b47b6ca178
Alejandro Puentes de busk <alejandro@192.168.1.16>
2025-04-09 19:47:41 -0600

    navegacion login

---
+++ ECommerceApp/src/navigation/AuthStack.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import WelcomeScreen from '../screens/WelcomeScreen';
+import LoginScreen from '../screens/Login';
+import RegisterScreen from '../screens/Register';
+import TabNavigator from './TabNavigator'; // para Home (después del login)
+
+const Stack = createNativeStackNavigator();
+
+export default function AuthStack() {
+  return (
+    <Stack.Navigator screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="Welcome" component={WelcomeScreen} />
+      <Stack.Screen name="Login" component={LoginScreen} />
+      <Stack.Screen name="Register" component={RegisterScreen} />
+      <Stack.Screen name="Home" component={TabNavigator} />
+    </Stack.Navigator>
+  );
+}
